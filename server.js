397ac035083571e3b2694560d4b3687537ba64aa
pusliaMary <pusliaprus@gmail.com>
2026-06-26 19:42:00 +0300

    dev(connection): connected to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const app = app.express()
-const PORT = 8000
+const app = express()
+const PORT = 8000 || process.env.PORT
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+mongoose.set('strictQuery', false)
+
+mongoose
+    .connect(process.env.MONGODB_LINK)
+    .then(()=> console.log("We are connected to Mongo"))
+    .catch(err => console.log(err)) 
 
 app.listen(PORT, ()=> {
     console.log(`I'm listening to port ${PORT}`)
