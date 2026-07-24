a3cfd30201699dff636f5b1ef100399385353fd8
pusliaMary <pusliaprus@gmail.com>
2026-07-24 20:31:50 +0300

    cors originals added

--- server.js
+++ server.js
@@ -8,6 +8,31 @@ require('dotenv').config()
 mongoose.set('strictQuery', false)
 const PORT = process.env.PORT || 8000
 
+const allowedOrigins = [
+    "https://easy-cooking-backend.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:8000"
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
+    credentials: true,
+};
+
+
+
+app.use(cors(corsOptions));
+//app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
