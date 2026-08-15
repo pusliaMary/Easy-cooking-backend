69d73ec88319b622d594704f3ba993bbbb408e33
pusliaMary <pusliaprus@gmail.com>
2026-08-15 20:20:21 +0300

    fix generateTokens

--- auth/auth.middleware.js
+++ auth/auth.middleware.js
@@ -1,5 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+// 🌟 ДОБАВЛЯЕМ ЭТУ ФУНКЦИЮ (она генерирует токен, который потом запишется в куку)
+module.exports.generateTokens = (username) => {
+  const accessToken = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '24h' });
+  return { accessToken };
+};
+
 module.exports.authenticate = (req, res, next) => {
   const token = req.cookies ? req.cookies.authToken : null;
 
